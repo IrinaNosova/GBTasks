8e7d348245352a9d5bde92cbf07d307b7b712752
Irina <irinka_n-1982@mail.ru>
2022-12-23 19:23:00 +0500

    Решени задачи 13

---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+
+ while (number > 1000)
+{
+    number = number/10;
+}
+WriteLine(number%10);
+if (number < 100)
+{
+    WriteLine("Третьей цифры нет");
+
+}

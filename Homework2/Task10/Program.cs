de35d4d12d9f9942efc50f8e88ac0457351b4a9f
Irina <irinka_n-1982@mail.ru>
2022-12-23 15:57:42 +0500

    Решение задачи 10

---
+++ Homework2/Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine(number < 1000 && number > 99? number % 100 / 10 : "Введено не трёхзначное число");

ba1f74e65e101b29ee731f4e5eccaaed1c43c192
Irina <irinka_n-1982@mail.ru>
2022-12-23 12:57:48 +0500

    Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,4 +5,4 @@ int a = Convert.ToInt32(ReadLine());
 WriteLine("Введите число b: ");
 int b = Convert.ToInt32(ReadLine());
 
-WriteLine(a%b == 0? "кратно":$"некратно");
+WriteLine(a%b == 0? "кратно":$"некратно, остаток {a%b}");

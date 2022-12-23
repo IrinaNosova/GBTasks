68a6d0065bdf74290a677b07bbc5b555e8ab8ecd
Irina <irinka_n-1982@mail.ru>
2022-12-23 13:51:43 +0500

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+WriteLine("Является ли одно число квадратом другого числа?");
+Write("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine(a * a == b || b * b == a? "является" : "не является");

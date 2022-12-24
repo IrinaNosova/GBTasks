1de3a8590c817fe0303594631fb0bbbafad682fe
Irina <irinka_n-1982@mail.ru>
2022-12-24 21:31:16 +0500

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int day = Convert.ToInt32(ReadLine());
+
+switch(day)
+{
+    case 1:
+    {
+        WriteLine("Понедельник");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("Вторник");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("Срела");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("Четверг");
+        break;
+    } 
+     case 5:
+    {
+        WriteLine("Пятница");
+        break;
+    }
+    case 6:
+    {
+        WriteLine("Суббота");
+        break;
+    }
+    case 7:
+    {
+        WriteLine("Воскресенье");
+        break;
+    } 
+    default:
+    {
+        Write("Данного дня недели не существует");
+        break;
+    }
+}
+

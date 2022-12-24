fbbd01e6c773258f8a019cfd74fc1b05ae530cf4
Irina <irinka_n-1982@mail.ru>
2022-12-24 22:07:59 +0500

    Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите координату х: ");
+int x = int.Parse(ReadLine()!);
+Write("Введите координату y: ");
+int y = int.Parse(ReadLine()!);
+
+{
+    if(x > 0 && y > 0)
+    {
+        WriteLine("1 четверть");
+    }
+    if(x < 0 && y > 0)
+    {
+        WriteLine("2 четверть");
+    }
+    if(x < 0 && y < 0)
+    {
+        WriteLine("третья четверть");
+    }
+    if(x > 0 && y < 0)
+    {
+        WriteLine("четвертая четверть");
+    }
+else
+    {
+       WriteLine("Ошибка");
+    }
+}

a4593fc7b1fd5917be73d749bde880e4c78126cb
Irina <irinka_n-1982@mail.ru>
2022-12-28 01:14:41 +0500

    Task23

---
+++ Homework3/Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+int i = 1;
+Write("Введите число: ");
+int.TryParse(ReadLine(),out int n);
+
+ while (i < n+1)
+{
+    WriteLine($"Квадрат числа{i} = {Math.Pow(i, 3)}");
+    i ++;
+}

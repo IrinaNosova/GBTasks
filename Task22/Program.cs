c1620b39213118429eb65f324cdc71921ec43aa2
Irina <irinka_n-1982@mail.ru>
2022-12-27 11:10:13 +0500

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Write("Точка А - Введите координату x: ");
+int.TryParse(ReadLine(),out int ax);
+Write("Точка А - Введите координату y: ");
+int.TryParse(ReadLine(),out int ay);
+Write("Точка Б - Введите координату x: ");
+int.TryParse(ReadLine(),out int bx);
+Write("Точка Б - Введите координату y: ");
+int.TryParse(ReadLine(),out int by);
+
+double length = Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2));
+
+WriteLine($"{length:f2}");

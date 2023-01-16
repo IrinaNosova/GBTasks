45d98901649ad9cef74bc976dc1fb6bbef65dd21
Irina Nosova <irinka_n-1982@mail.ru>
2023-01-16 23:49:43 +0500

    Решение задачи 25

---
+++ Homework4/Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// В задаче не использвать стандартный метод Pow!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+using static System.Console;
+
+Write("Введите превое число a: ");
+int.TryParse(ReadLine(), out int a);
+Write("Введите число b: ");
+int.TryParse(ReadLine(), out int b);
+
+WriteLine($"{a} в степени {b} = {VozvStep(a,b)}");
+
+
+int VozvStep (int A,int B)
+{
+    if (A == 0) return 0;
+    int result = 1;
+    for (int i=0; i<B; i++)
+    result *= A;
+    return result;
+}

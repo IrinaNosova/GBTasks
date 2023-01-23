1324ae19f28da27de199955efb7904fe3cc10a29
Irina <irinka_n-1982@mail.ru>
2023-01-23 10:46:37 +0500

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+int num = askNumber("Ввведите число для преревода в двоичную систему: ");
+
+WriteLine($"Десятичное число {num} в двоичном представлении будет {decimalToBinary(num)}");
+
+string decimalToBinary(int inNum)
+{
+ if (inNum== 0) return "0";   
+string result = string.Empty;
+
+int rem;      
+while (inNum >0)
+{
+    
+    rem = inNum % 2;
+    inNum /= 2;
+    result = rem.ToString() + result;
+}
+return result;
+}
+
+
+int askNumber(string inPrompt)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while(!int.TryParse(userInput, out resultNum))
+    {
+        Console.Write(inPrompt);
+        userInput= Console.ReadLine()!;
+    }
+    return resultNum;
+}

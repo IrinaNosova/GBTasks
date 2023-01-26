cf094ccac5697d9aea0ddc7ba98566bf95428401
Irina <irinka_n-1982@mail.ru>
2023-01-26 13:27:22 +0500

    Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите размер матрицы и диапозон значений через пробел: ");
+string[] parametrs = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+int[,] array = GetMatrixArray(int.Parse(parametrs[0]), int.Parse(parametrs[1]), int.Parse(parametrs[1]), int.Parse(parametrs[3]));
+
+PrintMatrixArray(array);
+PowMatrixArray(array);
+WriteLine();
+PrintMatrixArray(array);
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
+
+void PowMatrixArray(int[,] inArray)
+{
+    for (int i = 1; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 1; j < inArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) inArray[i, j] *= inArray[i, j];
+        }
+    }
+}

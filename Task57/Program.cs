2f80eb45bfa0784012e2c1bb7cd1956b366d59cf
Irina <irinka_n-1982@mail.ru>
2023-01-30 15:42:43 +0500

    Task57

---
+++ Task57/Program.cs
@@ -0,0 +1,95 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь 
+// содержит информацию о том, сколько раз встречается элемент входных данных.
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+using static System.Console;
+Clear();
+WriteLine("Введите размер матрицы и диапозон значений через пробел: ");
+string[] parametrs = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetMatrixArray(int.Parse(parametrs[0]), int.Parse(parametrs[1]), int.Parse(parametrs[2]), int.Parse(parametrs[3]));
+
+PrintMatrixArray(array);
+CountNumber(array);
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
+
+void CountNumber(int[,] inArray)
+{
+    int[] result = new int[inArray.GetLength(0) * inArray.GetLength(1)];
+    int a = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[a] = inArray[i, j];
+            a++;
+        }
+
+    }
+    
+    int temp = 0;
+    for (int i = 0; i < result.Length - 1; i++)
+    {
+        for (int j = i + 1; j < result.Length; j++)
+        {
+            if (result[i] > result[j])
+            {
+                temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+    
+    int num = result[0];
+    int count = 1;
+
+    for (int i = 1; i < result.Length; i++)
+    {
+        if (result[i] == num) count++;
+        else
+        {
+            WriteLine($"  {result[i - 1]} встречается {count} раз");
+            num = result[i];
+            count = 1;
+        }
+        if (i == result.Length - 1) WriteLine($"  {result[i ]} встречается {count} раз");
+    }
+}

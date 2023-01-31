80ad9f630195e0ed15c641f317ca3a46e767b39f
Irina <irinka_n-1982@mail.ru>
2023-01-31 17:11:17 +0500

    Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать 
+// сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+WriteLine($"Сумма цифр числа {number} = {SumElements(number)}");
+
+int SumElements(int num)
+{
+    if (num == 0) return num;
+    else return SumElements(num/10) + num%10;
+}

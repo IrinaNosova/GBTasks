f5ea04a63dc075d4a64660717c2d369df0806c48
Irina <irinka_n-1982@mail.ru>
2023-01-31 11:58:06 +0500

    Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+using static System.Console;
+
+Clear();
+string result = GetNumbers(10);
+WriteLine(result);
+string result1 = GetNumbers1(10);
+WriteLine(result1);
+PrintNumbers(10);
+
+string GetNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return $"{GetNumbers(num - 1)},{num}";
+}
+
+// Теранарный оператор
+string GetNumbers1(int num)
+{
+    return num==1?num.ToString(): $"{GetNumbers1(num - 1)},{num}";
+
+// Второй способ решения
+}
+void PrintNumbers(int num)
+{
+    if (num == 1) Write(num);
+    else
+    {
+        PrintNumbers(num - 1);
+        Write($",{num}");
+    }
+}
